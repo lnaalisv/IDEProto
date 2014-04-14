54aa3ef1a25aa9f2caa93e84b9d2aeca036cbbb9
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2014-04-14 15:00:23 +0300

    aasd

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 	add_author("Mickey","Mouse");
 	add_author("Uncle","Scrooge");
 	$(".dp").datepicker();
+	$("#title-error").hide();
+	//$("#tabs").tabs();
 });
 
 
@@ -21,4 +23,19 @@ function add_new_author_click(event) {
 	var bb = $("#lname").val();
 	add_author(aa,bb);
 	$("#author_modal").modal("hide");
+}
+
+function preview_and_save() {
+	var title_form_group = $("#title-form-group");
+	var title = $("#title");
+	var title_val = title.val();
+	if(typeof title_val == "undefined" || title_val == "") {
+		title_form_group.addClass("has-error");
+		title_form_group.addClass("has-feedback");
+		$("#title-error").show();
+	} else {
+		title_form_group.removeClass("has-error");
+		title_form_group.addClass("has-feedback");
+		$("#title-error").hide();
+	}
 }
