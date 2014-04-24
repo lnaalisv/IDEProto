a5553cc65a6260f8c4dd6f61c3623c5c46ffe6de
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2014-04-24 17:55:27 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -48,14 +48,21 @@ $(document).ready(function() {
 	data["Activity"] = {};
 	
 function select_click() {
-	
+	var item = $("#type-select3").val();
+	if(item == "Article") {
+		$("#main-add").toggleClass("active");
+		$("#new-article").toggleClass("active");;
+	}
 }
 
 function import_bibtex() {
 	$("#file-input").val("");
-	$("#output-data").prepend("<div class='row'><h1>Getting to know computer science freshmen</h1><p><b>Authors: </b>Kinnunen, Päivi and Marttila-Kontio, Maija and Pesonen, Erkki<b>2013</b></p></div><hr/>");
-	$("#output-data").prepend("<div class='row'><h1>Arguments for an information-centric internetworking architecture</h1><p><b>Authors: </b>Trossen, Dirk and Sarela, Mikko and Sollins, Karen<b>2010</b></p></div><hr/>");
-	$("#output-data").prepend("<div class='row'><h1>A Survey of Ethernet LAN Security</h1><p><b>Authors: </b>Kiravuo, Timo and Sarela, Mikko and Manner, Jukka<b>2013</b></p></div><hr/>");
+}
+function import_bibtex() {
+// <div class='col-sm-2'></div><div class='col-sm-4'><h3><span class='glyphicon glyphicon-ok'> Freshly imported</h3></div>
+	$("#output-data").prepend("<div class='row imported-row'><div class='col-sm-6'><h1>Getting to know computer science freshmen</h1><p><b>Authors: </b>Kinnunen, Päivi and Marttila-Kontio, Maija and Pesonen, Erkki<br/><b>2013</b></p></div><div class='col-sm-2'></div><div class='col-sm-4'><h3><span class='glyphicon glyphicon-ok'> Freshly imported</h3></div></div>");
+	$("#output-data").prepend("<div class='row imported-row'><div class='col-sm-6'><h1>Arguments for an information-centric internetworking architecture</h1><p><b>Authors: </b>Trossen, Dirk and Sarela, Mikko and Sollins, Karen<b>2010</b></p></div><div class='col-sm-2'></div><div class='col-sm-4'><h3><span class='glyphicon glyphicon-ok'> Freshly imported</h3></div></div>");
+	$("#output-data").prepend("<div class='row imported-row'><div class='col-sm-6'><h1>A Survey of Ethernet LAN Security</h1><p><b>Authors: </b>Kiravuo, Timo and Sarela, Mikko and Manner, Jukka<b>2013</b></p></div><div class='col-sm-2'></div><div class='col-sm-4'><h3><span class='glyphicon glyphicon-ok'> Freshly imported</h3></div></div>");
 }
 
 function type_select() {
